a2000d18006430b50338f4f07e27331b46b8282e
joansalinas <joan_sz@hotmail.com>
2021-07-23 13:28:14 +0200

    Modificada la pantalla principal

--- src/Screens/homeScreen.js
+++ src/Screens/homeScreen.js
@@ -1,62 +1,108 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, StatusBar, SafeAreaView, FlatList, Image, ScrollView, Button,Dimensions,TouchableHighlight, TouchableOpacity   } from 'react-native';
-import { Input, CheckBox, Overlay, ListItem, Icon } from 'react-native-elements';
+import { Input, CheckBox, Overlay, ListItem } from 'react-native-elements';
 import { postOrder } from '../api';
 const departments = ["frozen", "other", "bakery", "produce", "alcohol", "international", "beverages", "pets", "dry goods pasta", "bulk", "personal care", "meat seafood", "pantry", "breakfast", "canned goods", "dairy eggs", "household", "babies", "snacks", "deli", "missing"]
 
 export function HomeScreen() {
 
-	const [dataSource, setDataSource] = useState([]);
-	const [expandedIndex, setExpandedIndex] = useState(2)
-	useState(() => {
-		let items = Array.apply(null, Array(10)).map((v, i) => {
-			return {
-				id: i,
-				src: 'http://placehold.it/200x200?text=' + (i + 1)
-			};
-		});
-		setDataSource(items);
-	}, []);
+	const [products, setProducts] = useState([{'name':'patata', 'timeAverage': 5}]);
+	const [pantalla, setPantalla] = useState(0)
+	const [name, setName] = useState();
+	const [timeAverage, setTimeAverage] = useState();
+	const addProductGoToProducts = async () => {
+		const newList = products.concat({ name, timeAverage });
+		let product = {'name':name, 'timeAverage': timeAverage}
+		setProducts(newList)
+		goToProducts()
+		return
+		try{
 
+			let response = await postOrder("producte", "avg")
+			if (response && response.error){
+				alert("Error del servidor")
+			}
+			else{
+				setProducts([...products, product])
+				goToProducts()
+			};
+		}
+		catch(e){
+			alert("Error del servidor")
+		}
+	};
+
+	const goToProducts = async () => {
+		setPantalla(1)
+	};
+
+	const goToAddProducts = async () => {
+		setPantalla(0)
+	};
+	const Item = ({ title }) => (
+		<View style={styles.item}>
+			<Text style={styles.titol}>{name}</Text>
+		</View>
+	);
+	const renderItem = ({ item }) => (
+		<Item title={item.name} />
+	);
 	return (
 		<View style={styles.container}>
 			<Text style = {styles.titol}>Manducare</Text>
-			<Text style = {styles.subTitol}>Introdueix les dades del nou tiquet</Text>
-			<View style={styles.inputContainer}>
-				<StatusBar barStyle="light-content"/>
-				<Input label="Nom del producte" containerStyle={{marginTop:15}} style={styles.input} />
-				<Input label="Cada quants dies compres aquest producte aproximadament?" keyboardType="numeric"/>
-			</View>
-			<SafeAreaView style={styles.container}>
-				<ScrollView style={styles.inputContainer}>
-					{departments.map((l, i) => (
-						<View>
-							<Text>- {l}</Text>
+			{pantalla == 0 ? (
+					<View>
+						<Text style = {styles.subTitol}>Introdueix les dades del nou tiquet</Text>
+						<View style={styles.inputContainer}>
+							<StatusBar barStyle="light-content"/>
+							<Input 
+								label="Nom del producte" 
+								containerStyle={{marginTop:15}} 
+								style={styles.input} 
+								placeholder="Escriu aqui el teu producte"
+								onChangeText={name => setName({name})}
+		          			/>
+							<Input 	
+								label="Cada quants dies compres aquest producte aproximadament?" 
+								keyboardType="numeric"
+								style={styles.input} 
+								placeholder="Escriu aqui un valor numeric"
+								onChangeText={timeAverage => setTimeAverage({timeAverage})}
+							/>
 						</View>
-					))}
-				</ScrollView>
-
-			</SafeAreaView>
-			<View style={styles.inputContainer}>  
-				<Button color='#e6aa12' title='Afegir i veure productes similars'  onPress={() => this._submit(context)}/>
-			</View>
-
+						<View >  
+							<View style={styles.openButton}>
+								<Button style = {styles.openButton} color='#e6aa12'  title='Afegir i veure productes similars'  onPress={() => addProductGoToProducts()}/>
+							</View>
+							<View style={styles.openButton}>
+								<Button color='#e6aa12' style = {styles.openButton} title='Veure productes similars'  onPress={() => goToProducts()}/>
+							</View>
+						</View>
+					</View>
+				):
+				(
+					<View>
+						<Text style = {styles.subTitol}>Introdueix les dades del nou tiquet</Text>
+						<View >
+							<FlatList 
+								data={products}
+								renderItem={renderItem}
+								keyExtractor = {item => item.name}
+							/>
+							
+							<View style={styles.openButton}>
+								<Button style = {styles.openButton} color='#e6aa12'  title='Afegir un altre producte'  onPress={() => goToAddProducts()}/>
+							</View>
+						</View>
+					</View>
+				)
+			}
 		</View>
 	);
+	
+	
 }
-const _submit = async (context) => {
-	this.setState({
-		isSubmitting: true,
-		errorMessage: null
-	});
-	let response = await postOrder("producte", "avg")
-	if (response && response.error){
-		console.log("no ok")
-	}
-	else{
-		console.log("ok")
-	};
-};
+
 
 const size = Dimensions.get('window').width/3;
 const styles = StyleSheet.create({
@@ -94,35 +140,26 @@ const styles = StyleSheet.create({
 		alignItems: "center",
 		marginTop: 22
 	},
-
 	inputContainer:{
+		justifyContent: 'space-between',
 		marginHorizontal:20,
 		paddingTop:20,
 		paddingBottom:20
+	},
+	buttonsContainer:{
+		justifyContent: 'space-between',
+		marginHorizontal:20,
+		paddingTop:20,
 	},
 	itemContainer: {
 		width: 30,
 		height: 100,
 	},
-	modalView: {
-		margin: 20,
-		backgroundColor: "white",
-		borderRadius: 10,
-		padding: 35,
-		alignItems: "center",
-		shadowColor: "black",
-		shadowOffset: {
-				width: 1,
-			height: 2
-		},
-		shadowOpacity: 0.25,
-		shadowRadius: 3.84,
-		elevation: 5
-	},
 	openButton: {
-		backgroundColor: "#F194FF",
+		margin:20,
+		padding:5,
+		backgroundColor: "tomato",
 		borderRadius: 10,
-		padding: 10,
 		elevation: 2, 
 	},
 	textStyle: {
